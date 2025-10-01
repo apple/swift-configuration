aa05b081b1d5649cc0f0c119c2d1191d778ff34e
Finn Voorhees <finnvoorhees@gmail.com>
2025-10-01 13:05:48 +0100

    Expose AbsoluteConfigKey prepending/appending

--- Examples/hello-world-cli-example/Package.swift
+++ Examples/hello-world-cli-example/Package.swift
@@ -8,14 +8,18 @@ let package = Package(
         .macOS(.v15)
     ],
     dependencies: [
-       .package(url: "https://github.com/apple/swift-configuration", .upToNextMinor(from: "0.1.0"), traits: [.defaults, "CommandLineArgumentsSupport"])
+        .package(
+            url: "https://github.com/apple/swift-configuration",
+            .upToNextMinor(from: "0.1.0"),
+            traits: [.defaults, "CommandLineArgumentsSupport"]
+        )
     ],
     targets: [
         .executableTarget(
             name: "CLI",
             dependencies: [
-                .product(name: "Configuration", package: "swift-configuration"),
+                .product(name: "Configuration", package: "swift-configuration")
             ]
-        ),
+        )
     ]
 )
