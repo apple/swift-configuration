50d19231dc69f6eafdc60b1db97afd3c4ff81a1c
Honza Dvorsky <honza@apple.com>
2025-11-03 11:24:50 +0100

    Fix license headers in Deprecations.swift

--- Sources/Configuration/Deprecations.swift
+++ Sources/Configuration/Deprecations.swift
@@ -1,3 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftConfiguration open source project
+//
+// Copyright (c) 2025 Apple Inc. and the SwiftConfiguration project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftConfiguration project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 #if JSONSupport
 /// A provider of configuration in JSON files.
 @available(Configuration 1.0, *)
