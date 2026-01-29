c46e4bb3e066f67804944bdd3e65d7ffdd5265c0
Joe Heck <j_heck@apple.com>
2026-01-28 17:05:39 -0800

    initial working sample - tested in 'kind'

---
+++ Examples/reloading-example/Sources/App/App+build.swift
@@ -0,0 +1,50 @@
+import Configuration
+import Hummingbird
+import Logging
+
+// Request context used by application
+typealias AppRequestContext = BasicRequestContext
+
+///  Build application
+/// - Parameter reader: configuration reader
+func buildApplication(reader: ConfigReader) async throws -> some ApplicationProtocol {
+    let logger = {
+        var logger = Logger(label: reader.string(forKey: "http.serverName", default: "default-HB-server"))
+        logger.logLevel = reader.string(forKey: "log.level", as: Logger.Level.self, default: .info)
+        return logger
+    }()
+
+    let dynamicConfig = try await ReloadingFileProvider<JSONSnapshot>(config: reader)
+    // https://swiftpackageindex.com/apple/swift-configuration/1.0.1/documentation/configuration
+    // default reload - checks every 15 seconds (polling)
+    let dynamicConfigReader = ConfigReader(provider: dynamicConfig)
+    
+    let router = try buildRouter(config: reader, dynamicConfig: dynamicConfigReader)
+
+    // Alt - create app and add a service to it...
+    // https://docs.hummingbird.codes/2.0/documentation/hummingbird/servicelifecycle#Hummingbird-Integration
+    let app = Application(
+        router: router,
+        configuration: ApplicationConfiguration(reader: reader.scoped(to: "http")),
+        services: [dynamicConfig],
+        logger: logger
+    )
+    return app
+}
+
+/// Build router
+func buildRouter(config: ConfigReader, dynamicConfig: ConfigReader) throws -> Router<AppRequestContext> {
+    let router = Router(context: AppRequestContext.self)
+    // Add middleware
+    router.addMiddleware {
+        // logging middleware
+        LogRequestsMiddleware(config.string(forKey: "log.level", as: Logger.Level.self, default: .info))
+    }
+    // Add default endpoint
+    router.get("/") { _,_ in
+        let name = dynamicConfig.string(forKey: "name")
+
+        return "Hello \(name ?? "World")!"
+    }
+    return router
+}
